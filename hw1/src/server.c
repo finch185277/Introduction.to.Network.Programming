4bd5bc07fcaab838e13dd0a28efbab6224717035
finch185277 <59487andy@gmail.com>
2019-10-30 00:18:22 +0800

    Update server.c

---
+++ hw1/src/server.c
@@ -0,0 +1,14 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("Usage: %s <SERVER PORT>\n", argv[0]);
+    exit(0);
+  }
+  return 0;
+}
