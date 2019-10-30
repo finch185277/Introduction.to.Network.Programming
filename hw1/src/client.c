5391ec1fa79f98fd7c69a7379e4c1f1e8f8a280d
finch185277 <59487andy@gmail.com>
2019-10-30 09:40:07 +0800

    Update client.c

--- hw1/src/client.c
+++ hw1/src/client.c
@@ -7,19 +7,14 @@
 
 #define LINE_MAX 1024
 
-int max(int a, int b) {
-  if (a > b)
-    return a;
-  else
-    return b;
-}
-
 int main(int argc, char **argv) {
   if (argc != 3) {
     printf("Usage: %s <SERVER IP> <SERVER PORT>\n", argv[0]);
     exit(0);
   }
 
+  int sock_fd;
+  char buf[LINE_MAX];
   struct sockaddr_in srv_addr;
 
   memset(&srv_addr, 0, sizeof(srv_addr));
@@ -27,13 +22,13 @@ int main(int argc, char **argv) {
   srv_addr.sin_addr.s_addr = inet_addr(argv[1]);
   srv_addr.sin_port = htons(atoi(argv[2]));
 
-  int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+  sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   connect(sock_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
 
-  int max_fd = (sock_fd, STDIN_FILENO) + 1;
+  int max_fd = (sock_fd > STDIN_FILENO ? sock_fd : STDIN_FILENO) + 1;
   fd_set rset;
-  char buf[LINE_MAX];
+  FD_ZERO(&rset);
 
   for (;;) {
     FD_SET(STDIN_FILENO, &rset);
@@ -41,21 +36,24 @@ int main(int argc, char **argv) {
     select(max_fd, &rset, NULL, NULL, NULL);
 
     if (FD_ISSET(STDIN_FILENO, &rset)) {
-      int n = read(STDIN_FILENO, buf, LINE_MAX);
+      int n = read(STDIN_FILENO, buf, LINE_MAX - 1);
+      buf[n] = '\0';
       if (strcmp(buf, "exit\n") == 0) {
         close(sock_fd);
         break;
       }
       write(sock_fd, buf, n);
+    }
 
-    } else if (FD_ISSET(sock_fd, &rset)) {
-      int n = read(sock_fd, buf, LINE_MAX);
+    if (FD_ISSET(sock_fd, &rset)) {
+      int n = read(sock_fd, buf, LINE_MAX - 1);
       if (n == 0) {
-        printf("Connection closed by server");
+        printf("Connection closed by server\n");
         close(sock_fd);
         break;
       }
-      fprintf(stdout, "%s", buf);
+      buf[n] = '\0';
+      fprintf(stdout, "%s\n", buf);
     }
   }
 
