b7ae3f60b61f5d5ecf33cf7eac08e557b8b29473
finch185277 <59487andy@gmail.com>
2019-10-30 21:28:30 +0800

    Update server.c
    - rename func

--- hw1/src/server.c
+++ hw1/src/server.c
@@ -6,8 +6,8 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define LINE_MAX 1024
 #define CLIENT_MAX 10
+#define LINE_MAX 1024
 
 struct Client {
   int fd;
@@ -16,36 +16,40 @@ struct Client {
   int port;
 };
 
-struct Client clients[CLIENT_MAX];
-
-void send_msg(int fd, char *msg) {
+void msg_send(int fd, char *msg) {
   char buf[LINE_MAX];
   sprintf(buf, "[Server] %s\n", msg);
   write(fd, buf, strlen(buf));
 }
 
-void broadcast(struct Client *clients, int idx_bound, char *msg) {
+void msg_broadcast(struct Client *clients, int idx_bound, char *msg) {
   for (int i = 0; i <= idx_bound; i++)
     if (clients[i].fd >= 0)
-      send_msg(clients[i].fd, msg);
+      msg_send(clients[i].fd, msg);
 }
 
-void come(struct Client *clients, int idx, struct sockaddr_in *cli_addr) {
+void user_come(struct Client *clients, int idx, struct sockaddr_in *cli_addr) {
   char buf[LINE_MAX];
-  strcpy(clients[idx].name, "anonymous");
 
+  strcpy(clients[idx].name, "anonymous");
   inet_ntop(AF_INET, &cli_addr->sin_addr, clients[idx].ip,
             sizeof(clients[idx].ip));
-  clients[idx].port = ntohs(ntohs(cli_addr->sin_port));
+  clients[idx].port = ntohs(cli_addr->sin_port);
 
   sprintf(buf, "Hello, anonymous! From: %s:%d", clients[idx].ip,
           clients[idx].port);
-  send_msg(clients[idx].fd, buf);
+  msg_send(clients[idx].fd, buf);
 }
 
-void name(struct Client *clients, int idx, int idx_bound, char *name) {
+void user_leave(struct Client *clients, int idx, int idx_bound) {
+  char buf[LINE_MAX];
+  sprintf(buf, "%s is offline.", clients[idx].name);
+  msg_broadcast(clients, idx_bound, buf);
+}
+
+void cmd_name(struct Client *clients, int idx, int idx_bound, char *name) {
   if (strcmp(name, "anonymous") == 0) {
-    send_msg(clients[idx].fd, "ERROR: Username cannot be anonymous.");
+    msg_send(clients[idx].fd, "ERROR: Username cannot be anonymous.");
     return;
   }
   for (int i = 0; i <= idx_bound; i++) {
@@ -53,7 +57,7 @@ void name(struct Client *clients, int idx, int idx_bound, char *name) {
         strcmp(name, clients[idx].name) == 0) {
       char buf[LINE_MAX];
       sprintf(buf, "ERROR: %s has been used by others.", name);
-      send_msg(clients[idx].fd, buf);
+      msg_send(clients[idx].fd, buf);
       return;
     }
   }
@@ -61,7 +65,7 @@ void name(struct Client *clients, int idx, int idx_bound, char *name) {
   if (name_len < 2 || name_len > 12 ||
       strspn(name, "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz") !=
           name_len) {
-    send_msg(clients[idx].fd,
+    msg_send(clients[idx].fd,
              "ERROR: Username can only consists of 2~12 English letters.");
     return;
   }
@@ -69,25 +73,25 @@ void name(struct Client *clients, int idx, int idx_bound, char *name) {
   char buf[LINE_MAX];
 
   sprintf(buf, "You're now known as %s.", name);
-  send_msg(clients[idx].fd, buf);
+  msg_send(clients[idx].fd, buf);
 
   int fd = clients[idx].fd;
   clients[idx].fd = -1;
   sprintf(buf, "%s is now known as %s.", clients[idx].name, name);
-  broadcast(clients, idx_bound, buf);
+  msg_broadcast(clients, idx_bound, buf);
   clients[idx].fd = fd;
 
   strcpy(clients[idx].name, name);
 }
 
-void tell(struct Client *clients, int idx, int idx_bound, char *name,
-          char *msg) {
+void cmd_tell(struct Client *clients, int idx, int idx_bound, char *name,
+              char *msg) {
   if (strcmp(clients[idx].name, "anonymous") == 0) {
-    send_msg(clients[idx].fd, "ERROR: You are anonymous.");
+    msg_send(clients[idx].fd, "ERROR: You are anonymous.");
     return;
   }
   if (strcmp(name, "anonymous") == 0) {
-    send_msg(clients[idx].fd,
+    msg_send(clients[idx].fd,
              "ERROR: The client to which you sent is anonymous.");
     return;
   }
@@ -95,23 +99,23 @@ void tell(struct Client *clients, int idx, int idx_bound, char *name,
     if (clients[i].fd >= 0 && strcmp(clients[i].name, name) == 0) {
       char buf[LINE_MAX];
 
-      send_msg(clients[idx].fd, "SUCCESS: Your message has been sent.");
+      msg_send(clients[idx].fd, "SUCCESS: Your message has been sent.");
 
       sprintf(buf, "%s tell you %s", clients[idx].name, msg);
-      send_msg(clients[i].fd, buf);
+      msg_send(clients[i].fd, buf);
       return;
     }
   }
-  send_msg(clients[idx].fd, "ERROR: The receiver doesn't exist.");
+  msg_send(clients[idx].fd, "ERROR: The receiver doesn't exist.");
 }
 
-void yell(struct Client *clients, int idx, int idx_bound, char *msg) {
+void cmd_yell(struct Client *clients, int idx, int idx_bound, char *msg) {
   char buf[LINE_MAX];
   sprintf(buf, "%s yell %s", clients[idx].name, msg);
-  broadcast(clients, idx_bound, buf);
+  msg_broadcast(clients, idx_bound, buf);
 }
 
-void who(struct Client *clients, int idx, int idx_bound) {
+void cmd_who(struct Client *clients, int idx, int idx_bound) {
   for (int i = 0; i <= idx_bound; i++) {
     char buf[LINE_MAX];
     if (clients[i].fd >= 0) {
@@ -119,15 +123,9 @@ void who(struct Client *clients, int idx, int idx_bound) {
               clients[idx].port);
       if (i == idx)
         strcat(buf, " ->me");
-      send_msg(clients[i].fd, buf);
+      msg_send(clients[i].fd, buf);
     }
   }
-}
-
-void leave(struct Client *clients, int idx, int idx_bound) {
-  char buf[LINE_MAX];
-  sprintf(buf, "%s is offline.", clients[idx].name);
-  broadcast(clients, idx_bound, buf);
 }
 
 int main(int argc, char **argv) {
@@ -135,5 +133,8 @@ int main(int argc, char **argv) {
     printf("Usage: %s <SERVER PORT>\n", argv[0]);
     exit(0);
   }
+
+  struct Client clients[CLIENT_MAX];
+
   return 0;
 }
