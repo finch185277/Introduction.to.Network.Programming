48c56f36ca85619c44477d73c7cb3e14035f8bb3
finch185277 <59487andy@gmail.com>
2019-10-31 20:05:25 +0800

    Update client.c

--- hw1/src/client.c
+++ hw1/src/client.c
@@ -5,7 +5,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define LINE_MAX 1024
+#include "chat-room.h"
 
 int main(int argc, char **argv) {
   if (argc != 3) {
@@ -13,29 +13,28 @@ int main(int argc, char **argv) {
     exit(0);
   }
 
-  int sock_fd;
-  char buf[LINE_MAX];
   struct sockaddr_in srv_addr;
-
   memset(&srv_addr, 0, sizeof(srv_addr));
   srv_addr.sin_family = AF_INET;
   srv_addr.sin_addr.s_addr = inet_addr(argv[1]);
   srv_addr.sin_port = htons(atoi(argv[2]));
 
+  int sock_fd;
   sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   connect(sock_fd, (struct sockaddr *)&srv_addr, sizeof(srv_addr));
 
+  fd_set r_set;
+  FD_ZERO(&r_set);
   int max_fd = (sock_fd > STDIN_FILENO ? sock_fd : STDIN_FILENO) + 1;
-  fd_set rset;
-  FD_ZERO(&rset);
 
   for (;;) {
-    FD_SET(STDIN_FILENO, &rset);
-    FD_SET(sock_fd, &rset);
-    select(max_fd, &rset, NULL, NULL, NULL);
+    FD_SET(STDIN_FILENO, &r_set);
+    FD_SET(sock_fd, &r_set);
+    select(max_fd, &r_set, NULL, NULL, NULL);
 
-    if (FD_ISSET(STDIN_FILENO, &rset)) {
+    if (FD_ISSET(STDIN_FILENO, &r_set)) {
+      char buf[LINE_MAX];
       int n = read(STDIN_FILENO, buf, LINE_MAX - 1);
       buf[n] = '\0';
       if (strcmp(buf, "exit\n") == 0) {
@@ -45,7 +44,8 @@ int main(int argc, char **argv) {
       write(sock_fd, buf, n);
     }
 
-    if (FD_ISSET(sock_fd, &rset)) {
+    if (FD_ISSET(sock_fd, &r_set)) {
+      char buf[LINE_MAX];
       int n = read(sock_fd, buf, LINE_MAX - 1);
       if (n == 0) {
         printf("Connection closed by server\n");
