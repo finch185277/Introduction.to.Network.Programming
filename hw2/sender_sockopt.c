f3dfd0236cc71fdd59083e6d66d395a5cc656f6b
finch185277 <59487andy@gmail.com>
2019-12-07 21:41:23 +0800

    Update sockopt

---
+++ hw2/sender_sockopt.c
@@ -0,0 +1,24 @@
+// client
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1000
+
+struct frame {
+  long int ID;
+  long int length;
+  char data[BUF_SIZE];
+};
+
+int main(int argc, char *argv[]) {
+  if (argc != 4) {
+    printf("Usage: %s [send filename] [target address] [connect port]\n",
+           argv[0]);
+    exit(0);
+  }
+
+  return 0;
+}
