d5bc0227b9947b92fc4a4005c3568e8b4e2f6ac9
finch185277 <59487andy@gmail.com>
2019-12-07 23:52:26 +0800

    Update sockopt

--- hw2/receiver_sockopt.c
+++ hw2/receiver_sockopt.c
@@ -7,7 +7,7 @@
 
 #define BUF_SIZE 1000
 
-struct frame {
+struct frame_t {
   long int ID;
   long int length;
   char data[BUF_SIZE];
@@ -17,6 +17,57 @@ int main(int argc, char *argv[]) {
   if (argc != 3) {
     printf("Usage: %s [save filename] [bind port]\n", argv[0]);
     exit(0);
+  }
+
+  struct sockaddr_in listen_addr;
+  memset(&listen_addr, 0, sizeof(listen_addr));
+  listen_addr.sin_family = AF_INET;
+  listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  listen_addr.sin_port = htons(atoi(argv[2]));
+
+  int listen_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  bind(listen_fd, (struct sockaddr *)&listen_addr, sizeof(listen_addr));
+
+  struct sockaddr_in cli_addr;
+  int sock_len = sizeof(cli_addr);
+  long int total_frame = 0, bytes_recv = 0;
+
+  // get total frame
+  struct timeval timeout;
+  timeout.tv_sec = 2;
+  setsockopt(listen_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+             sizeof(struct timeval));
+
+  recvfrom(listen_fd, &total_frame, sizeof(total_frame), 0,
+           (struct sockaddr *)&cli_addr, (socklen_t *)&sock_len);
+
+  timeout.tv_sec = 0;
+  setsockopt(listen_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+             sizeof(struct timeval));
+
+  sendto(listen_fd, &total_frame, sizeof(total_frame), 0,
+         (struct sockaddr *)&cli_addr, sizeof(cli_addr));
+
+  for (int idx = 0; idx < total_frame;) {
+    struct frame_t frame;
+    recvfrom(listen_fd, &frame, sizeof(frame), 0, (struct sockaddr *)&cli_addr,
+             (socklen_t *)&sock_len);
+    sendto(listen_fd, &frame.ID, sizeof(frame.ID), 0,
+           (struct sockaddr *)&cli_addr, sizeof(cli_addr));
+
+    if (frame.ID == idx) {
+      FILE *file = fopen(argv[1], "a");
+      fwrite(frame.data, sizeof(char), frame.length, file);
+      fclose(file);
+      bytes_recv += frame.length;
+      printf("Bytes received %ld\n", bytes_recv);
+      idx++;
+    }
+
+    if (idx == total_frame) {
+      printf("File transfer success!\n");
+    }
   }
 
   return 0;
--- hw2/sender_sockopt.c
+++ hw2/sender_sockopt.c
@@ -4,10 +4,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #define BUF_SIZE 1000
 
-struct frame {
+struct frame_t {
   long int ID;
   long int length;
   char data[BUF_SIZE];
@@ -20,5 +22,72 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
+  struct sockaddr_in srv_addr;
+  memset(&srv_addr, 0, sizeof(srv_addr));
+  srv_addr.sin_family = AF_INET;
+  srv_addr.sin_addr.s_addr = inet_addr(argv[2]);
+  srv_addr.sin_port = htons(atoi(argv[3]));
+
+  int sock_fd;
+  sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+  if (access(argv[1], F_OK) == 0) {
+    struct stat st;
+    stat(argv[1], &st);
+    int file_size = st.st_size;
+
+    struct timeval timeout;
+    timeout.tv_sec = 2;
+    setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+               sizeof(struct timeval));
+
+    int total_frame;
+    if (file_size % BUF_SIZE == 0) {
+      total_frame = file_size / BUF_SIZE;
+    } else {
+      total_frame = file_size / BUF_SIZE + 1;
+    }
+
+    int ack_num;
+    struct sockaddr_in cli_addr;
+    int sock_len = sizeof(cli_addr);
+
+    while (ack_num != total_frame) {
+      sendto(sock_fd, &total_frame, sizeof(total_frame), 0,
+             (struct sockaddr *)&srv_addr, sizeof(srv_addr));
+      recvfrom(sock_fd, &ack_num, sizeof(ack_num), 0,
+               (struct sockaddr *)&cli_addr, (socklen_t *)&sock_len);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    for (int idx = 0; idx < total_frame;) {
+      struct frame_t frame;
+      frame.ID = idx;
+      frame.length = fread(frame.data, sizeof(char), BUF_SIZE, file);
+
+      sendto(sock_fd, &frame, sizeof(frame), 0, (struct sockaddr *)&srv_addr,
+             sizeof(srv_addr));
+      recvfrom(sock_fd, &ack_num, sizeof(ack_num), 0,
+               (struct sockaddr *)&cli_addr, (socklen_t *)&sock_len);
+
+      while (ack_num != frame.ID) {
+        sendto(sock_fd, &frame, sizeof(frame), 0, (struct sockaddr *)&srv_addr,
+               sizeof(srv_addr));
+        recvfrom(sock_fd, &ack_num, sizeof(ack_num), 0,
+                 (struct sockaddr *)&cli_addr, (socklen_t *)&sock_len);
+      }
+
+      idx++;
+
+      if (idx == total_frame) {
+        printf("File transfer success!\n");
+      }
+    }
+    fclose(file);
+
+    timeout.tv_sec = 0;
+    setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, &timeout,
+               sizeof(struct timeval));
+  }
   return 0;
 }
