576154359913835621e93c3a8c352087f7db7ade
finch185277 <59487andy@gmail.com>
2019-12-29 15:10:13 +0800

    Update client.cpp

--- hw3/client.cpp
+++ hw3/client.cpp
@@ -9,21 +9,20 @@
 #define LINE_MAX 1024
 
 int main(int argc, char **argv) {
-  if (argc != 3) {
+  if (argc != 4) {
     printf("Usage: %s <SERVER IP> <SERVER PORT> <USERNAME>\n", argv[0]);
     exit(0);
   }
 
   printf("Welcome to the dropbox-like server: %s\n", argv[3]);
 
-  struct hostent *host = gethostbyname(argv[1]);
-
   struct sockaddr_in srv_addr;
   memset(&srv_addr, 0, sizeof(srv_addr));
   srv_addr.sin_family = AF_INET;
-  srv_addr.sin_addr.s_addr = inet_addr(argv[1]);
-  memcpy(&srv_addr.sin_addr, host->h_addr, host->h_length);
   srv_addr.sin_port = htons(atoi(argv[2]));
+
+  struct hostent *host = gethostbyname(argv[1]);
+  memcpy(&srv_addr.sin_addr, host->h_addr, host->h_length);
 
   int sock_fd;
   sock_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -53,7 +52,8 @@ int main(int argc, char **argv) {
 
     if (FD_ISSET(sock_fd, &r_set)) {
       char buf[LINE_MAX];
-      int n = read(sock_fd, buf, LINE_MAX);
+      int n = read(sock_fd, buf, LINE_MAX - 1);
+      buf[n] = '\0';
       if (n == 0) {
         close(sock_fd);
         break;
