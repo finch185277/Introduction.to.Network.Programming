89af6719355159063fe9ce9f8be7f2a562499a91
finch185277 <59487andy@gmail.com>
2020-01-09 11:11:42 +0800

    Update final

--- final/p1.c
+++ final/p1.c
@@ -10,77 +10,73 @@
 #include <unistd.h>
 
 char *sockaddr2str(const struct sockaddr *sa, int flags) {
-	char host[128];
-	char serv[16];
+  char host[128];
+  char serv[16];
 
-	socklen_t salen = sizeof(struct sockaddr_in);
-	int n = getnameinfo(sa, salen, host, sizeof(host), serv, sizeof(serv), flags);
+  socklen_t salen = sizeof(struct sockaddr_in);
+  int n = getnameinfo(sa, salen, host, sizeof(host), serv, sizeof(serv), flags);
 
-	if (n != 0)
-		return NULL;
+  if (n != 0)
+    return NULL;
 
-	char *ret = malloc(200 * sizeof(char));
-	sprintf(ret, "%s", host);
+  char *ret = malloc(200 * sizeof(char));
+  sprintf(ret, "%s", host);
 
-	return ret;
+  return ret;
 }
 
 int print_host(const char *hostname) {
-	struct addrinfo *result = NULL;
-	struct addrinfo hint;
+  struct addrinfo *result = NULL;
+  struct addrinfo hint;
 
-	memset(&hint, 0, sizeof(hint));
-	hint.ai_socktype = SOCK_STREAM;
-	int n = getaddrinfo(hostname, NULL, &hint, &result);
+  memset(&hint, 0, sizeof(hint));
+  hint.ai_socktype = SOCK_STREAM;
+  int n = getaddrinfo(hostname, NULL, &hint, &result);
 
-	if (n == 0) {
-		struct addrinfo *cur = result;
-		unsigned cnt = 0;
+  if (n == 0) {
+    struct addrinfo *cur = result;
+    unsigned cnt = 0;
 
-		struct sockaddr_in sock_addr;
-		int req_flag = inet_pton(AF_INET, hostname, &sock_addr.sin_addr);
+    struct sockaddr_in sock_addr;
+    int req_flag = inet_pton(AF_INET, hostname, &sock_addr.sin_addr);
 
-		if(cur){
-			char *dotted, *revhost;
-			dotted = sockaddr2str(cur->ai_addr, NI_NUMERICHOST | NI_NUMERICSERV);
-			revhost = sockaddr2str(cur->ai_addr, NI_NAMEREQD | NI_NUMERICSERV);
+    if (cur) {
+      char *dotted, *revhost;
+      dotted = sockaddr2str(cur->ai_addr, NI_NUMERICHOST | NI_NUMERICSERV);
+      revhost = sockaddr2str(cur->ai_addr, NI_NAMEREQD | NI_NUMERICSERV);
 
-			if (req_flag) {
-				if (revhost) {
-					puts(revhost);
-					free(revhost);
-				}else{
-					printf("Can't find %s\n", hostname);
-				}
-			} else {
-				if (dotted) {
-					puts(dotted);
-					free(dotted);
-				}else{
-					printf("Can't find %s\n", hostname);
-				}
+      if (req_flag) {
+        if (revhost) {
+          puts(revhost);
+          free(revhost);
+        } else {
+          printf("Can't find %s\n", hostname);
+        }
+      } else {
+        if (dotted) {
+          puts(dotted);
+          free(dotted);
+        } else {
+          printf("Can't find %s\n", hostname);
+        }
+      }
+    }
+  } else {
+    printf("Can't find %s\n", hostname);
+  }
 
-			}
-
-		}
-	} else {
-		printf("Can't find %s\n", hostname);
-	}
-
-
-	freeaddrinfo(result);
-	return n;
-
+  freeaddrinfo(result);
+  return n;
 }
 
 int main(int argc, char **argv) {
-	for(;;) {
-		printf("> ");
-		char req[200];
-		scanf("%s", req);
-		if(strcmp(req, "exit") == 0)
-			break;
-		print_host(req);
-	}
-	return 0;
+  for (;;) {
+    printf("> ");
+    char req[200];
+    scanf("%s", req);
+    if (strcmp(req, "exit") == 0)
+      break;
+    print_host(req);
+  }
+  return 0;
 }
--- final/p2c.c
+++ final/p2c.c
@@ -1,7 +1,7 @@
 #include <arpa/inet.h>
+#include <errno.h>
 #include <netdb.h>
 #include <signal.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,62 +11,62 @@
 #define LINE_MAX 1000
 
 void handler(int signo) {
-	wait(NULL);
-	return;
+  wait(NULL);
+  return;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		printf("Usage: %s <server IP> <server port>\n", argv[0]);
-		exit(0);
-	}
+  if (argc != 3) {
+    printf("Usage: %s <server IP> <server port>\n", argv[0]);
+    exit(0);
+  }
 
-	struct sockaddr_in srv_addr;
-	memset(&srv_addr, 0, sizeof(srv_addr));
-	srv_addr.sin_family = AF_INET;
-	srv_addr.sin_port = htons(atoi(argv[2]));
+  struct sockaddr_in srv_addr;
+  memset(&srv_addr, 0, sizeof(srv_addr));
+  srv_addr.sin_family = AF_INET;
+  srv_addr.sin_port = htons(atoi(argv[2]));
 
-	struct hostent *host = gethostbyname(argv[1]);
-	memcpy(&srv_addr.sin_addr, host->h_addr, host->h_length);
+  struct hostent *host = gethostbyname(argv[1]);
+  memcpy(&srv_addr.sin_addr, host->h_addr, host->h_length);
 
-	int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
+  int sock_fd = socket(AF_INET, SOCK_DGRAM, 0);
 
-	connect(sock_fd, (const struct sockaddr *)&srv_addr, sizeof(srv_addr));
+  connect(sock_fd, (const struct sockaddr *)&srv_addr, sizeof(srv_addr));
 
-	signal(SIGCHLD, handler);
+  signal(SIGCHLD, handler);
 
-	pid_t pid = fork();
-	if (pid == 0) { // child process
-		for (;;) {
-			char buf[LINE_MAX];
-			int n = recv(sock_fd, buf, LINE_MAX - 1, 0);
-			if (n == 0) // EOF
-				break;
-			if (n < 0){
-				printf("errno = %d from sock_fd\n", errno);
-			}
-			buf[n] = '\0';
-			printf("%s", buf);
-		}
-	} else { // parent process
-		for (;;) {
-			char buf[LINE_MAX];
-			int n = read(STDIN_FILENO, buf, LINE_MAX - 1);
-			if (n == 0) // EOF
-				break;
-			buf[n] = '\0';
-			if(strcmp(buf, "exit\n") == 0) {
-				close(sock_fd);
-				break;
-			}
-			if(strcmp(buf, "GetProgress\n") == 0) {
-				write(sock_fd, buf, n);
-			}
-		}
-		char command[LINE_MAX];
-		sprintf(command, "kill %d", pid);
-		system(command);
-	}
+  pid_t pid = fork();
+  if (pid == 0) { // child process
+    for (;;) {
+      char buf[LINE_MAX];
+      int n = recv(sock_fd, buf, LINE_MAX - 1, 0);
+      if (n == 0) // EOF
+        break;
+      if (n < 0) {
+        printf("errno = %d from sock_fd\n", errno);
+      }
+      buf[n] = '\0';
+      printf("%s", buf);
+    }
+  } else { // parent process
+    for (;;) {
+      char buf[LINE_MAX];
+      int n = read(STDIN_FILENO, buf, LINE_MAX - 1);
+      if (n == 0) // EOF
+        break;
+      buf[n] = '\0';
+      if (strcmp(buf, "exit\n") == 0) {
+        close(sock_fd);
+        break;
+      }
+      if (strcmp(buf, "GetProgress\n") == 0) {
+        write(sock_fd, buf, n);
+      }
+    }
+    char command[LINE_MAX];
+    sprintf(command, "kill %d", pid);
+    system(command);
+  }
 
-	return 0;
+  return 0;
 }
